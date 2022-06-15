d10d8c56bdfc2eef3e9c29e8b6ce8f7ca8409ea1
Tlaster <tlaster@outlook.com>
2022-06-15 10:43:07 +0800

    upgrade compose version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ buildscript {
 }
 
 plugins {
-    kotlin("multiplatform") version "1.6.10"
-    id("org.jetbrains.compose") version "1.2.0-alpha01-dev609"
+    kotlin("multiplatform") version "1.6.21"
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev713"
 }
 
 version = "1.0-SNAPSHOT"
@@ -84,7 +84,10 @@ kotlin {
 }
 
 compose.experimental {
-    uikit.application {}
+    uikit.application {
+        bundleIdPrefix = "moe.tlaster"
+        projectName = "ComposeBootstrap"
+    }
 }
 
 tasks.withType<KotlinCompile> {
